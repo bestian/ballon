6f103a02ffa1eba32b8886d44d1dc4a84e9b8261
Bestian Tang 小巴 <bestian@gmail.com>
2021-06-13 09:52:30 +0800

    init

---
+++ src/pages/Book.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class = "flex flex-center">
+    <a href="https://www.books.com.tw/products/0010476475" target="_blank" rel="noopener noreferrer">造型氣球（基礎篇）</a>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,8 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/9Y8mgHZBYQ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <br/>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/0bwSbwjYWFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </q-page>
 </template>
 
