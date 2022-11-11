5c41550819aad6d76cb5bd62e17931a5cb60fae7
Bestian Tang 小巴 <bestian@gmail.com>
2022-11-11 14:48:17 +0800

    s

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/-RK04LnHVYk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/9Y8mgHZBYQ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <br/>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/0bwSbwjYWFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
